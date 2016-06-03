8571a294b4e977b77b71b9ba7ebc31d33a2e432a
Robbie Wolfe <Robbie Wolfe>
2016-06-03 14:20:41 +0300

    Added the ability to ignore specific files or folders.
    Not every file or folder you add to version control you want on
    your web server. Creating a .svnignore file will tell the script
    to not touch it.
    
    	new file:   .svnignore
    	modified:   FTP.py
    	modified:   README.md

--- FTP.py
+++ FTP.py
@@ -12,9 +12,58 @@ DELETED         = "D   "
 UPDATED         = "U   "
 OFFSET          = len(ADDED) + len(TRUNK)
 
+IGNORE_FILE_NAME = ".svnignore"
+IGNORE_FILE_COMMENT = "#"
+
 LOCAL       = "C:\\My Site"
 FTP_SERVER  = "mysite.example.com"
 REMOTE_ROOT = "public_html/"
+
+def process_ignore_file(file_location):
+    ignore_list = []
+    try:
+        ignore_file = open(file_location)
+    except IOError:     # File doesn't exist, do nothing
+        print "No ignore file detected, proceeding to upload"
+    else:
+        try:
+            for line in ignore_file:
+                if not line.startswith(IGNORE_FILE_COMMENT):
+                    ignore_list.append(line.rstrip("\r\n"))
+        finally:
+            ignore_file.close()
+
+    return ignore_list
+
+def process_commit_list(repo, rev, ignore_list):
+    # Take SVN changed output and convert it to a list of Added/Deleted/Modified
+    changed = subprocess.check_output(["svnlook", "changed", "-r", rev, repo]).split("\r\n")[:-1]
+    added = []
+    deleted = []
+    updated = []
+    
+    for change in changed:
+        file_name = change[OFFSET:]
+        skip_this = False
+        
+        # Check if this change should be not be uploaded
+        for ignore in ignore_list:
+            if ignore in file_name:
+                print "Ignoring change: %s" % file_name
+                skip_this = True
+                break
+
+        if skip_this:
+            continue
+
+        if change.startswith(ADDED):
+            added.append(file_name)
+        elif change.startswith(DELETED):
+            deleted.append(file_name)
+        elif change.startswith(UPDATED):
+            updated.append(file_name)
+
+    return added, deleted, updated
 
 def upload(ftp, local_root, files):
     for file_name in files:
@@ -79,30 +128,20 @@ def delete(ftp, files):
         ftp.rmd(remote_path)
 
 if __name__ == "__main__":
+
     repo, rev = sys.argv[1:]
-	
+
+    local_root = os.path.join(LOCAL, TRUNK)
+
     if repo == "" or rev == "":
-        raise Exception("Empty repo or rev")
+        raise Exception("Empty repo or rev arguments")
+
+    ignore_list = process_ignore_file(os.path.join(local_root, IGNORE_FILE_NAME))
+
+    added, deleted, updated = process_commit_list(repo, rev, ignore_list)
     
     user = raw_input("Please enter your ftp username: ")
     password = getpass.getpass()
-
-    local_root = os.path.join(LOCAL, TRUNK)
-    
-    # Take SVN changed output and convert it to a list of Added/Deleted/Modified
-    changed = subprocess.check_output(["svnlook", "changed", "-r", rev, repo]).split("\r\n")[:-1]
-    added = []
-    deleted = []
-    updated = []
-    
-    for change in changed:
-        file_name = change[OFFSET:]
-        if change.startswith(ADDED):
-            added.append(file_name)
-        elif change.startswith(DELETED):
-            deleted.append(file_name)
-        elif change.startswith(UPDATED):
-            updated.append(file_name)
 
     ftp = FTP(FTP_SERVER)
     ftp.login(user, password)
--- README.md
+++ README.md
@@ -14,12 +14,23 @@ This script is very simple to install.
 
 1. Copy post-commit.bat and FTP.py to your svn\hooks\ folder.
 2. Modify the following variables in FTP.py
- - LOCAL        - The full path location to the local SVN repo
- - FTP_SERVER   - The full URL to the FTP server
- - REMOTE_ROOT  - The root directory to upload the repo to
+ - LOCAL        - The full path location to the SVN working copy (the folder containing trunk)
+ - FTP_SERVER   - The full URL to the server to upload to
+ - REMOTE_ROOT  - The root directory on the server to upload the repo to
 3. Make a commit, enter your username and password, and see the script run.
+
+## Preventing files from uploading
+Sometimes you have files that you want to be version controlled but you don't want
+on your FTP server. You can let the script know to ignore these files by creating
+a file called .svnignore in the trunk folder found in your main working copy folder. 
+Files or folders listed in this file will be ignored. Each new file or folder should
+be on its own line and spelled correctly. All files or folders should be written 
+relative to the folder containing the svnignore file. Files should include their extension. 
+Folders are indicated with a / at the end of the name. You can add comments by 
+starting the line with a #. The ignore file itself is automatically ignored.
 
 ## TODO
 - Modify post-commit.bat so the command prompt closes automatically
  - Add a debug variable to make this easily configurable
 - Fix bug when "adding" a file that already exists on the server
+ - This would be a sign something is out of sync and maybe the script should abort either way
